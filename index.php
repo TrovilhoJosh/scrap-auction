c936822a50cb0a4d2c24738414b0ae990a836d58
TrovilhoJosh <78884647+TrovilhoJosh@users.noreply.github.com>
2023-06-29 20:45:03 -0300

    get title from website

--- index.php
+++ index.php
@@ -6,12 +6,25 @@
   <title>Scrap Auction</title>
 </head>
 <body>
-  
-  <?php 
-  
-    echo "Scrap Auction";
-  
-  ?>
+
+<?php 
+
+libxml_use_internal_errors(true);
+
+$conteudo = file_get_contents('https://agostinholeiloes.com.br/item/1435/detalhes?page=1');
+
+$documento = new DOMDocument();
+$documento->loadHTML($conteudo);
+
+$xPath = new DOMXPath($documento);
+$domNodeList = $xPath->query('.//*[contains(concat(" ",normalize-space(@class)," ")," container ")][contains(concat(" ",normalize-space(@class)," ")," detalhes-lote ")]//h4/following-sibling::*[1]/self::h4');
+
+/** @var DOMNode $elemento */
+foreach ($domNodeList as $elemento) {
+  echo $elemento->textContent . PHP_EOL."<br><br>";
+}
+
+?>
 
 </body>
 </html>
